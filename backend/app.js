bb46a7a0027f931adc12346d93cedbc679fc5ead
psswineford <psswineford@gmail.com>
2023-03-30 14:20:49 -0700

    connected to mongo DB and added posting and deleting operations

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post')
 
 const app = express();
+
+mongoose.connect("mongodb+srv://meanAdmin:theGr8one@myflixdb.h4dbi.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to the database');
+})
+.catch(() => {
+  console.log('Connection Failed!');
+});
 
 app.use(bodyParser.json());
 
 app.use((req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept");
-  res.setHeader("Access-Control-Allow-Methds", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
+  });
+
 });
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fasd123",
-      title: "First Server side post",
-      content: "This is content from the server",
-    },
-    {
-      id: "asfa123",
-      title: "Second Server side post",
-      content: "This is content from the server also",
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
+
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
   });
+  res.status(200).json({message: "Post deleted!"});
 });
 
 module.exports = app;
