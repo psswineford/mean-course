7158b2f1cdef8611a9aabb0c064f46eed2d40b7b
psswineford <psswineford@gmail.com>
2023-04-10 13:06:13 -0700

    updated routes on backend and added loading spinners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './components/post-create/post-create.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostListComponent } from './components/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 import {MatInputModule} from '@angular/material/input';
@@ -15,7 +16,7 @@ import {MatCardModule} from '@angular/material/card'
 import {MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
-import { AppRoutingModule } from './app-routing.module';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -35,6 +36,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/service/posts.service.ts
+++ src/app/service/posts.service.ts
@@ -3,7 +3,9 @@ import { Subject } from 'rxjs';
 import { Post } from '../Models/post.model';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
-import { response } from 'express';
+import { Router } from '@angular/router';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getPosts() {
     this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
@@ -47,6 +49,7 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
       });
   }
 
@@ -59,6 +62,7 @@ export class PostsService {
       updatedPosts[oldPostIndex] = post;
       this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
+      this.router.navigate(["/"]);
     });
   }
 
